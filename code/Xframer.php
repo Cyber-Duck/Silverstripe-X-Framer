308137ad80fb1c08e826a51307831bafcef320e8
Andrew McCormack <andrewm@cyber-duck.co.uk>
2016-06-27 16:30:00 +0100

    add core

--- code/Xframer.php
+++ code/Xframer.php
@@ -8,13 +8,54 @@
  **/
 class Xframer {
 
-	function init()
+	private static $ip;
+
+	private static $excluded = array();
+
+	private static $headers = array(
+		'HTTP_CLIENT_IP', 
+		'HTTP_X_FORWARDED_FOR', 
+		'HTTP_X_FORWARDED', 
+		'HTTP_X_CLUSTER_CLIENT_IP', 
+		'HTTP_FORWARDED_FOR', 
+		'HTTP_FORWARDED', 
+		'REMOTE_ADDR'
+	);
+
+	public static function init($header = 'SAMEORIGIN')
 	{
-		$this->getUserIP();
+		self::getUserIP();
+		self::getExcludedIPs();
+
+		if(!in_array(self::$ip, self::$excluded))
+		{
+			if(is_object(Controller::curr()->response))
+			{
+				Controller::curr()->response->addHeader('X-Frame-Options', $header);
+			}
+		}
 	}
 
-	private function getUserIP()
+	private static function getUserIP()
 	{
-		
+		$ips = [];
+
+		foreach(self::$headers as $header)
+		{
+			if(array_key_exists($header, $_SERVER) === true)
+			{
+				foreach(explode(',', $_SERVER[$header]) as $ip) $ips[] = trim($ip);
+			}
+		}
+
+		if(!empty($ips))
+		{
+			self::$ip = implode(',', $ips);
+		}
+	}
+
+	private static function getExcludedIPs()
+	{
+		self::$excluded = Config::inst()->get('Xframer','ips');
 	}
 }
