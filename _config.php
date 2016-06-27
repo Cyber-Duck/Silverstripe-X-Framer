7b66d5942bdca1febc40c9dc5823274054860a41
Andrew McCormack <andrewm@cyber-duck.co.uk>
2016-06-27 16:04:08 +0100

    Add composer

---
+++ _config.php
@@ -0,0 +1,2 @@
+<?php
+if(!defined('XFRAMER_PATH')) define('XFRAMER_PATH', rtrim(basename(dirname(__FILE__))));
---
+++ code/Xframer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Silverstripe X-Framer
+ *
+ * @package silverstripe-x-framer
+ * @license MIT License https://github.com/Cyber-Duck/silverstripe-x-framer/blob/master/LICENSE
+ * @author  <andrewm@cyber-duck.co.uk>
+ **/
+class Xframer {
+
+	function init()
+	{
+		$this->getUserIP();
+	}
+
+	private function getUserIP()
+	{
+		
+	}
+}
