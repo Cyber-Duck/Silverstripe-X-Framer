f635cb00765e3c907560a273e737e46680715965
Andrew Mc Cormack <andrewm@cyber-duck.co.uk>
2016-12-08 20:26:03 +0000

    Update code styling

--- code/Xframer.php
+++ code/Xframer.php
@@ -6,28 +6,32 @@
  * @license MIT License https://github.com/Cyber-Duck/Silverstripe-X-Framer/blob/master/LICENSE
  * @author  <andrewm@cyber-duck.co.uk>
  **/
-class Xframer {
-
+class Xframer
+{
     /**
+     * The current user IP address
+     *
      * @since version 1.0.0
      *
-     * @static string $ip The current user IP address
+     * @static string $ip
      **/
     private static $ip;
 
     /**
+     * An array of IP addresses to exclude from returning X-Frame header
+     *
      * @since version 1.0.0
      *
-     * @static array $excluded An array of IP addresses to exclude from returning X-Frame header
+     * @static array $excluded
      **/
-    private static $excluded = array();
+    private static $excluded = [];
     
     /**
      * @since version 1.0.0
      *
      * @static array $headers An array of server headers to check for an IP
      **/
-    private static $headers = array(
+    private static $headers = [
         'HTTP_CLIENT_IP', 
         'HTTP_X_FORWARDED_FOR', 
         'HTTP_X_FORWARDED', 
@@ -35,7 +39,7 @@ class Xframer {
         'HTTP_FORWARDED_FOR', 
         'HTTP_FORWARDED', 
         'REMOTE_ADDR'
-    );
+    ];
 
     /**
      * Run the plugin by setting and checking the current user IP and setting
@@ -43,7 +47,7 @@ class Xframer {
      *
      * @since version 1.0.0
      *
-     * @param string $header X-Frame option DENY | SAMEORIGIN
+     * @param string $header X-Frame option DENY|SAMEORIGIN
      *
      * @return void
      **/
@@ -52,10 +56,8 @@ class Xframer {
         self::setUserIP();
         self::setExcludedIPs();
 
-        if(!in_array(self::$ip, self::$excluded))
-        {
-            if(is_object(Controller::curr()->response))
-            {
+        if(!in_array(self::$ip, self::$excluded)) {
+            if(is_object(Controller::curr()->response)) {
                 Controller::curr()->response->addHeader('X-Frame-Options', $header);
             }
         }
@@ -70,19 +72,16 @@ class Xframer {
      **/
     private static function setUserIP()
     {
-        $ips = [];
+        foreach(self::$headers as $header) {
+            if(array_key_exists($header, $_SERVER)) {
+                foreach (explode(',', $_SERVER[$header]) as $ip) {
+                    $ip = trim($ip);
 
-        foreach(self::$headers as $header)
-        {
-            if(array_key_exists($header, $_SERVER) === true)
-            {
-                foreach(explode(',', $_SERVER[$header]) as $ip) $ips[] = trim($ip);
+                    if(filter_var($ip, FILTER_VALIDATE_IP)) {
+                        return self::$ip = $ip;
+                    }
+                }
             }
-        }
-
-        if(!empty($ips))
-        {
-            self::$ip = implode(',', $ips);
         }
     }
 
